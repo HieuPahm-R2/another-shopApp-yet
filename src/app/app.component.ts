4e3b767c71be6b42064b30a9cc7eac8779e76ec0
HieuPahmTr <phamvanthang786@gmail.com>
2025-04-29 17:20:54 +0700

    29/04/2025 - #design login&register form + design product detail page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { OrderComponent } from './order/order.component';
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from "./login/login.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @Component({
   selector: 'app-root',
@@ -14,7 +16,9 @@ import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
     HomeComponent,
     OrderComponent,
     OrderConfirmComponent,
-  ],
+    LoginComponent,
+    ProductDetailComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
