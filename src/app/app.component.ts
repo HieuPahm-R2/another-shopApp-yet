a05a2bee160e56b192ba7ae53ac3bd392d9f316b
HieuPahmTr <phamvanthang786@gmail.com>
2025-04-29 16:14:00 +0700

    29/04/2025 - #Design Order confirm page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HomeComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
